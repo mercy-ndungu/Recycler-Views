d7e0f8e0d414245aa4b1195291217fdb1bc31270
mercy-ndungu <mercyndungukirigo@gmail.com>
2022-07-16 08:01:13 +0300

    added view binding to my recycler adapter

--- app/src/main/java/com/kirigo/mycontact/RecyclerAdapter.kt
+++ app/src/main/java/com/kirigo/mycontact/RecyclerAdapter.kt
@@ -6,17 +6,21 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
+import com.kirigo.mycontact.databinding.ContactListItemBinding
 
 class RecyclerAdapter (var contact: List<Contact>):
 RecyclerView.Adapter<ContactViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-        var itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.contact_list_item,parent,false)
-        return ContactViewHolder(itemView)
+        var binding=
+           ContactListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        var ContactViewHolder= ContactViewHolder(binding)
+        return ContactViewHolder
     }
 
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int){
         var currentcontact = contact.get(position)
+
+//        var currentcontact = contact.get(position)
         holder.tvName.text = currentcontact.name
         holder.tvDetails.text = currentcontact.PhoneNumber
 
